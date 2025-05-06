ee24d7eccc74643c1f14c6efb0751e1882583fba
alisam132 <ali.habib132@gmail.com>
2025-05-06 20:33:48 +0300

    add the secret key

--- carmaintenance/settings.py
+++ carmaintenance/settings.py
@@ -99,8 +99,8 @@ if 'ON_HEROKU' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'carmaintenance',
         }
     }
 
