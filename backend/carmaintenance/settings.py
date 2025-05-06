08da04df663aedf18c245665dda47b0f243e813e
alisam132 <ali.habib132@gmail.com>
2025-05-06 18:32:48 +0300

    add all requirments for deploying

--- backend/carmaintenance/settings.py
+++ backend/carmaintenance/settings.py
@@ -12,7 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
+import dj_database_url
 
+
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+@_7ck=+cdfeqyu*lxsc0l$13p12o6_5tp!#*0ksv1rsn$6y-a'
+# SECRET_KEY = 'django-insecure-+@_7ck=+cdfeqyu*lxsc0l$13p12o6_5tp!#*0ksv1rsn$6y-a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if not 'ON_HEROKU' in os.environ:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware"
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
@@ -80,10 +89,20 @@ WSGI_APPLICATION = 'carmaintenance.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    if 'ON_HEROKU' in os.environ:
+        DATABASES = {
+            "default": dj_database_url.config(
+                env='DATABASE_URL',
+                conn_max_age=600,
+                conn_health_checks=True,
+                ssl_require=True,
+            ),
+        }
+    else:
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'carmaintenance',
+        }
 }
 
 
@@ -122,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
